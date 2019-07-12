91d3099c8ae2afe7140a5f9574d6032da5b80102
许玥明 <xuyueming@beisen.com>
2019-07-12 18:00:45 +0800

    初始化

---
+++ GOF/GOF/Observer/Interfate/DisplayElement.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GOF.Observer
+{
+    public interface DisplayElement
+    {
+        void Display();
+    }
+}
---
+++ GOF/GOF/Observer/Interfate/ISubject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GOF.Observer
+{
+    public interface ISubject
+    {
+        void RegisterObserver(IObserver observer);
+
+        void RemoveObserver(IObserver observer);
+
+        void NotifyObserver();
+    }
+}
